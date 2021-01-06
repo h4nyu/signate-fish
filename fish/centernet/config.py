94558589f81aa5a536c858a52c407ee30c1a9e50
xinyuan-yao <yao.ntno@gmail.com>
2021-01-06 14:46:44 +0000

    start train

--- fish/centernet/config.py
+++ fish/centernet/config.py
@@ -1,20 +1,20 @@
 from typing import *
 from object_detection.model_loader import WatchMode
 
-out_dir = "store/0"
+out_dir = "store/1"
 
-image_size = 1024
+image_size = 1024 + 512
 num_classes = 2
 batch_size = 3
 
 # model
-backbone_idx = 1
-channels = 128
-out_idx = 5
-box_depth = 2
+backbone_idx = 3
+channels = 64
+out_idx = 4
+box_depth = 1
 
 # criterion
-lr = 1e-4
+lr = 1e-3
 box_weight = 1.0
 heatmap_weight = 1.0
 sigma = 20
