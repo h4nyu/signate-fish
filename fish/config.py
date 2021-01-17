90003802fb814f0c7c8c1e5d5074d73d47507559
xinyuan-yao <yao.ntno@gmail.com>
2021-01-17 05:43:40 +0000

    2048

--- fish/config.py
+++ fish/config.py
@@ -7,8 +7,8 @@ min_box_height = 10
 min_box_area = 200
 normalize_mean = (0.485, 0.456, 0.406)
 normalize_std = (0.485, 0.456, 0.406)
-# image_size = 1024 * 2
-image_size = 512 + 1024
+image_size = 1024 * 2
+# image_size = 512 + 1024
 scale = image_size / original_width
 num_classes = 2
 
