7947028c15e70bf1a3c126bb586a9fcecae80318
xinyuan-yao <yao.ntno@gmail.com>
2021-01-19 12:25:56 +0000

    fix ?

--- fish/effdet/config.py
+++ fish/effdet/config.py
@@ -18,8 +18,8 @@ batch_size = 4
 channels = 48
 box_depth = 1
 fpn_depth = 1
-lr = 1e-4
-out_ids: List[int] = [5, 6, 7]
+lr = 5e-4
+out_ids: List[int] = [6, 7]
 
 metric: Tuple[str, WatchMode] = ("score", "max")
 pretrained = True
