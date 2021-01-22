cbef4b2669100c685e5b56ec8fbd9c1836b5f369
xinyuan.yao <yao.ntno@gmail.com>
2021-01-23 01:33:29 +0900

    try many anchor

--- fish/effdet/config.py
+++ fish/effdet/config.py
@@ -18,7 +18,7 @@ channels = 64
 box_depth = 1
 fpn_depth = 1
 lr = 2e-4
-out_ids: List[int] = [5, 6, 7]
+out_ids: List[int] = [4, 5, 6, 7]
 
 metric: Tuple[str, WatchMode] = ("score", "max")
 pretrained = True
@@ -31,6 +31,6 @@ cls_weight = 1
 anchor_ratios = [0.7, 1.0, 1.3]
 anchor_scales = [1.0, 1.44]
 num_anchors = len(anchor_ratios) * len(anchor_scales)
-anchor_size = 2
+anchor_size = 3
 
 out_dir = f"/store/efficientdet-{num_anchors}-{channels}"
