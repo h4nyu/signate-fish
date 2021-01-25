cf1908478df19eb3e0243244ff65ca48f5ad4206
xinyuan.yao <yao.ntno@gmail.com>
2021-01-26 01:13:05 +0900

    ?

--- fish/centernet/config.py
+++ fish/centernet/config.py
@@ -6,7 +6,7 @@ from object_detection.models.mkmaps import GaussianMapMode
 out_dir = "store/centernet3"
 
 batch_size = 6
-mk_map_mode: GaussianMapMode = "aspect"
+mk_map_mode: GaussianMapMode = "length"
 
 # model
 channels = 64
@@ -15,16 +15,16 @@ cls_depth = 1
 box_depth = 1
 
 # criterion
-lr = 1e-4
+lr = 2e-4
 box_weight = 2.0
 heatmap_weight = 1.0
-sigma = 8
+sigma = 15
 metric: Tuple[str, WatchMode] = ("score", "max")
 
 # to_boxes
 to_boxes_threshold = 0.02
 iou_threshold = 0.2
-to_boxes_kernel_size = 9
+to_boxes_kernel_size = 7
 
 pseudo_threshold = 0.15
 
