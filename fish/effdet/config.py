7091d10d63f7b7a6918f3163d0e234e5800e4a79
xinyuan.yao <yao.ntno@gmail.com>
2021-02-02 22:17:38 +0900

    fix nan

--- fish/effdet/config.py
+++ fish/effdet/config.py
@@ -11,26 +11,26 @@ use_amp = True
 # data
 confidence_threshold = 0.001
 iou_threshold = 0.45
-batch_size = 3
+batch_size = 4
 
 # model
-channels = 32
+channels = 64
 box_depth = 1
 fpn_depth = 1
-lr = 1e-3
-out_ids: List[int] = [4, 5, 6, 7]
+lr = 5e-4
+out_ids: List[int] = [5, 6, 7]
 
 metric: Tuple[str, WatchMode] = ("score", "max")
 pretrained = True
 
 # criterion
-topk = 50
+topk = 19
 box_weight = 5
 cls_weight = 1
 
 anchor_ratios = [2/3, 1.0, 3/2]
 anchor_scales = [1.0]
 num_anchors = len(anchor_ratios) * len(anchor_scales)
-anchor_size = 8
+anchor_size = 4
 
 out_dir = f"/store/efficientdet-{num_anchors}-{channels}"
