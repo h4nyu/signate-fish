eaf0c880e91bdae59dd5dab059232b8e0b972f21
xinyuan.yao <yao.ntno@gmail.com>
2021-02-10 13:19:49 +0900

    more manybox

--- fish/effdet/config.py
+++ fish/effdet/config.py
@@ -18,7 +18,7 @@ channels = 64
 box_depth = 1
 fpn_depth = 1
 lr = 2e-4
-out_ids: List[int] = [7]
+out_ids: List[int] = [5, 6, 7]
 
 metric: Tuple[str, WatchMode] = ("score", "max")
 pretrained = True
